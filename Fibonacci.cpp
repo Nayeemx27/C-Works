81928a44133ed77962cc9331adec022386f90809
Nayeemx27 <88242287+Nayeemx27@users.noreply.github.com>
2021-10-08 13:41:16 +0600

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,25 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int fibo (int n)
+{
+    if (n<=1)
+            return n;
+
+    else
+        return fibo (n-1)+fibo(n-2);
+
+}
+
+int main ()
+{
+    int n;
+    cin>>n;
+    int sum= fibo(n);
+    cout<<sum;
+    return 0;
+}
+
+
